8bb9361f1015ef21c99a0a4c2a4c22438a3256f4
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-02-06 02:43:22 +0900

    refac : 퀵정렬 간소화

--- sorting(정렬)/QuickSort.js
+++ sorting(정렬)/QuickSort.js
@@ -68,3 +68,21 @@ function quickSortOpt(arr, left=0,right=arr.length-1){
     return arr;
 }
 console.log("result: ", quickSort([4,8,2,1,7, 5, 9,6,3]));
+
+//인터넷에서 찾음
+let arr = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8];
+const n = arr.length;
+
+const quickSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  const p = arr[0]; //피벗은 첫 번째 원소
+  const tail = arr.slice(1); //피벗을 제외한 배열
+
+  const leftArr = tail.filter((v) => v <= p); //분할된 왼쪽 부분 (피벗보다 작은 값)
+  const rightArr = tail.filter((v) => v > p); //분할된 오른쪽 부분 (피벗보다 큰 값)
+
+  return [...quickSort(leftArr), p, ...quickSort(rightArr)];
+};
+
+console.log(quickSort(arr, 0, n - 1));
