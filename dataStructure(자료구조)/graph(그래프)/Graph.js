7944f8b0f251c0d7807cb1f6e3d7600938fc593c
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-02-11 02:45:09 +0900

    Section27-Graph_Traversal

--- dataStructure(자료구조)/graph(그래프)/Graph.js
+++ dataStructure(자료구조)/graph(그래프)/Graph.js
@@ -1,3 +1,4 @@
+//Adjacency List
 class Graph {
     constructor() {
         this.adjacencyList = {};
@@ -24,7 +25,71 @@ class Graph {
         }
         delete this.adjacencyList[vertex];
     }
+    depthFirstRecursive(vertex){
+        let result = [];
+        let visited = {};
+        const adjacencyList = this.adjacencyList;
+        (function dfs(vertex){
+                if(!vertex) return null;
+                visited[vertex] = true;
+                result.push(vertex);
+                adjacencyList[vertex].forEach(neighbor => {
+                    console.log(neighbor);
+                    if(!visited[neighbor]){
+                        return dfs(neighbor);
+                    }
+                });
+                // for(let neighbor of adjacencyList[vertex]){
+                //     console.log(neighbor);
+                //     if(!visited[neighbor]){
+                //         dfs(neighbor);
+                //     }
+                // }
+            })(vertex);
+        return result;
+    }
+    depthFirstIterative(start){
+        let stack = [start];
+        let result = [];
+        let visited = {
+            [start] : true
+        };
+        let currentVertex;
+        while(stack.length){
+            currentVertex = stack.pop();
+            console.log(currentVertex);
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach((neighbor) => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            });
+        }
+        return result;  
+    }
+    breadthFirstSearch(start){
+        let queue =[start];
+        let result = [];
+        let visited = {
+            [start] : true,
+        };
+        let currentVertex;
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+            //반대 방향 this.adjacencyList[currentVertex].slice().reverse().forEach
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            })
+        }
+        return result;
+    }
 }
+
 let g = new Graph();
 
 g.addVertex("A")
@@ -43,6 +108,9 @@ g.addEdge("D","E")
 g.addEdge("D","F")
 g.addEdge("E","F")
 console.log(g.adjacencyList);
+// console.log(g.depthFirstRecursive("A"));
+console.log(g.depthFirstIterative("A"));
+// console.log(g.breadthFirstSearch("A"));
 
 //          A
 //        /   \
